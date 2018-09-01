9e6f3d851ce1283752e0830c82c265c4e8a937c6
Rahul Kumar <rahulkumar11501@gmmail.com>
2018-09-01 09:31:08 +0530

    working with function newCard

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	fmt.Println(card)
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	// card := "Ace of Spades"
+	// fmt.Println(card)
+	// card = "Five of Diamonds"
+	// fmt.Println(card)
+	card := newCard()
+}
+
+
+func newCard() string {
+	retunrn "Five of Diamonds"
 }
