94138821d649dfcbe737dcacc6e02ffd2e6c3965
Rahul Kumar <rahulkumar11501@gmmail.com>
2018-09-01 09:26:38 +0530

    variable declarations type

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	fmt.Println(card)
+	card = "Five of Diamonds"
+	fmt.Println(card)
 }
