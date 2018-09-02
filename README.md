967dba2db57fbb24572e0e3f84d5557ec8691f83
Rahul Kumar <rahulkumar11501@gmmail.com>
2018-09-02 09:03:25 +0530

    create function for the main deck added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cards
 Crad game project on Go
+
+Go is staticly typed language
+
+Aawesome Card game app coming soon
--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// fmt.Println(card)
 	// card = "Five of Diamonds"
 	// fmt.Println(card)
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
 	// }
-	fmt.Println(cards)
-
+	// fmt.Println(cards)
+	cards := newDeck()
 	cards.print()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
