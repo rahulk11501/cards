9fc327f41743a19ac70b48b1ef5398ec6567cc01
rahul kumar <rahulkumar11501@gmail.com>
2018-09-13 06:07:04 -0700

    WIP

--- main.go
+++ main.go
@@ -5,17 +5,6 @@ import (
 )
 
 func main() {
-	// var card string = "Ace of Spades"
-	// card := "Ace of Spades"
-	// fmt.Println(card)
-	// card = "Five of Diamonds"
-	// fmt.Println(card)
-	// cards := deck{"Ace of Diamonds", newCard()}
-	// cards = append(cards, "Six of Spades")
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
-	// fmt.Println(cards)
 	cards := newDeck()
 	cards.print()
 
